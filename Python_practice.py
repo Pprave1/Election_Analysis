4ef77fabfe90268cf791dced390cd51fe9dd3fcc
Your Name <you@example.com>
2021-12-11 12:49:02 -0500

    Adding files

---
+++ Python_practice.py
@@ -0,0 +1,13 @@
+# Python_practice.py
+counties = ["Arapahoe","Denver","jefferson"]
+if counties [1] == 'Denver':
+    print(counties[1])
+    print(counties[-1])
+    print(counties[0:2])
+    counties_dict = {}
+    counties_dict["Arapahoe"] = 422829
+    print(counties_dict)
+
+    counties_dict
+    print("Hello World")
+    
