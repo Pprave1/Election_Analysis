4c863885c3655423e5d5d92fb9350ced5b3918a7
Your Name <you@example.com>
2021-12-12 15:59:33 -0500

    Adding Files

--- PyPoll.py
+++ PyPoll.py
@@ -1,24 +1,52 @@
-
 import csv
 import os
 
 #  Assign a variable for the file to load and the path.
-file_to_load=os.path.join("D:\Priya\OsuBootCampModules\OSU Boot Camp\Module 3\Election_Analysis\Resources","election_results.csv")
-
-#  Open the election results and read the file.
+#file_to_load=os.path.join("D:\Priya\OsuBootCampModules\OSU Boot Camp\Module 3\Election_Analysis\Resources","election_results.csv")
+#file_to_load="D:\Priya\OsuBootCampModules\OSU Boot Camp\Module 3\Election_Analysis\Resources\election_results.csv"
+file_to_load=os.path.join("Resources","election_results.csv")
+#Create a filename variable to a direct or indirect path to the file.
+file_to_save = os.path.join("Resources\Analysis","election_analysis.txt")
+ 
+# Open the election results and read the file.
+#election_data = open(file_to_load, 'r')
 # with open function
-election_data = open(file_to_load, 'r')
-#with open(file_to_load) as election_data:
+with open(file_to_load)as election_data:
+
+#To do: perfrom analysis.
+#Close the file.
+#print(election_data)
+#election_data.close()
+
+# Using the open() function with the "w" mode we will write data to the file.
+#open(file_to_save, "w")
+#Use the open statement to open the file as a text file.
+#outfile = open(file_to_save, "w")
+#with open(file_to_save,"w") as txt_file:
+
+# Write some data to the file
+  #outfile.write("Hello World")
+  #txt_file.write("Hello World")
+  #Open the election results and read the file
+   #txt_file.write("Counties in the Election\n--------------------------\n")
+   #txt_file.write("Arapahoe,  ")
+   #txt_file.write("Denver, ")
+   #txt_file.write("Jefferson")
+   # Write three counties to the file.
+   #txt_file.write("\nArapahoe,Denver,Jefferson")
+   #txt_file.write("\nArapahoe\nDenver\nJefferson")
+# Close the file
+#outfile.close()
 # Read the file object with the reader function.
-file_reader = csv.reader(election_data)
+   file_reader = csv.reader(election_data)
 #Print the header row.
-headers = next(file_reader)
-print(headers)
-   #To do: perfrom analysis.
+   headers = next(file_reader)
+   print(headers)
+   
 #print (election_data)
 #Print each row in the CSV file. 
-#for row in  file_reader:
-#print(row)
+   #for row in  file_reader:
+     # print(row)
 
 
 
